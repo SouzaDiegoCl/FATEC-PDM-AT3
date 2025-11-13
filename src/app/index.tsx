1ef5ed37f025800d320bf82ab5afebeaf160aa82
SouzaDiegoCl <souzadiegocl@gmail.com>
2025-11-13 19:37:48 -0300

    feat: Finaliza Lista e adiciona titulo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -30,63 +30,63 @@ export default function Index() {
             nome: "Calabresa",
             descricao: "Mussarela, calabresa fatiada, cebola roxa e orégano.",
             valor: 42.00,
-            imagem: "https://cdn.deliway.com.br/blog/base/09a/7b0/2dc/pizza-de-calabresa.jpg",
+            imagem: "https://media.istockphoto.com/id/1276765965/pt/foto/tasty-pizza-and-ingredients-isolated-on-white-background.jpg?s=2048x2048&w=is&k=20&c=h4xE4Y0D4NvYRkdl7pwIj_ncUJUynPqPPN15uPurfdg=",
         },
         {
             id: 3,
             nome: "Frango com Catupiry",
             descricao: "Frango desfiado, catupiry original e milho verde.",
             valor: 45.50,
-            imagem: "https://www.receiteria.com.br/wp-content/uploads/receitas-de-pizza-de-frango-com-catupiry-1.jpg",
+            imagem: "https://images.unsplash.com/photo-1609159085820-d9a6ff0f469e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8",
         },
         {
             id: 4,
             nome: "Portuguesa",
             descricao: "Presunto, ovos, cebola, ervilha, azeitona e mussarela.",
             valor: 44.00,
-            imagem: "https://www.sabornamesa.com.br/media/k2/items/cache/62bff7f9aeb6138ad13b532d8b4a6f38_XL.jpg",
+            imagem: "https://images.unsplash.com/photo-1559978137-8c560d91e9e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDUwfHx8ZW58MHx8fHx8",
         },
         {
             id: 5,
             nome: "Quatro Queijos",
             descricao: "Mussarela, provolone, parmesão e gorgonzola.",
             valor: 46.90,
-            imagem: "https://www.sabornamesa.com.br/media/k2/items/cache/f4a21c04a42a671d289c68b61c484bb3_XL.jpg",
+            imagem: "https://plus.unsplash.com/premium_photo-1673439305009-821f62df6d31?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDQ4fHx8ZW58MHx8fHx8",
         },
         {
             id: 6,
             nome: "Pepperoni",
             descricao: "Molho de tomate, pepperoni, queijo mussarela e orégano.",
             valor: 49.90,
-            imagem: "https://www.receiteria.com.br/wp-content/uploads/pizza-de-pepperoni-01.jpg",
+            imagem: "https://images.unsplash.com/photo-1689793606720-be22e34c9322?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDQzfHx8ZW58MHx8fHx8",
         },
         {
             id: 7,
             nome: "Vegetariana",
             descricao: "Brócolis, palmito, pimentão, cebola, tomate e azeitonas.",
             valor: 43.00,
-            imagem: "https://www.receiteria.com.br/wp-content/uploads/pizza-vegetariana.jpg",
+            imagem: "https://images.unsplash.com/photo-1595378833389-86cc60210926?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI2fHx8ZW58MHx8fHx8",
         },
         {
             id: 8,
             nome: "Bacon com Cheddar",
             descricao: "Mussarela, bacon crocante e creme de cheddar.",
             valor: 48.00,
-            imagem: "https://www.receiteria.com.br/wp-content/uploads/pizza-de-bacon-com-cheddar-01.jpg",
+            imagem: "https://plus.unsplash.com/premium_photo-1675103910740-533375dd3864?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI0fHx8ZW58MHx8fHx8",
         },
         {
             id: 9,
             nome: "Napolitana",
             descricao: "Molho de tomate, mussarela, rodelas de tomate e orégano.",
             valor: 40.00,
-            imagem: "https://www.receiteria.com.br/wp-content/uploads/pizza-napolitana-01.jpg",
+            imagem: "https://images.unsplash.com/photo-1655471264223-b07ce84d521c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIyfHx8ZW58MHx8fHx8",
         },
         {
             id: 10,
             nome: "Chocolate com Morango",
             descricao: "Chocolate derretido e morangos frescos.",
             valor: 47.00,
-            imagem: "https://www.receiteria.com.br/wp-content/uploads/pizza-de-chocolate-com-morango-01.jpg",
+            imagem: "https://plus.unsplash.com/premium_photo-1664472735403-77644019b02e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHx8",
         }
     ]
 
@@ -100,14 +100,15 @@ export default function Index() {
                     flexDirection: "row",
                     alignItems: "center",
                     justifyContent: "flex-start",
-                    gap: 24
+                    gap: 24,
                 }}>
+
                     <Image source={item.imagem} style={{width: 100, height: 100, borderRadius: 20}}/>
-                    <View style={{ flexShrink: 1 }}>
+                    <View style={{flexShrink: 1}}>
                         <Text style={{
                             fontSize: 16,
                             fontWeight: "bold",
-                            color: "white",
+                            color: "000",
                             marginBottom: 4,
                             flexWrap: "wrap",
                         }}>
@@ -116,7 +117,7 @@ export default function Index() {
                         <Text style={{
                             fontSize: 14,
                             fontWeight: "normal",
-                            color: "white",
+                            color: "000",
                             marginBottom: 4,
                             flexWrap: "wrap",
                         }}>
@@ -139,7 +140,17 @@ export default function Index() {
     }
 
     return (
-        <FlatList data={pizzarias_data} renderItem={render_list_item}/>
+        <>
+            <Text style={{textAlign: "center", paddingVertical: 24, fontWeight: "bold", fontSize: 24}} >Lista de Pizza</Text>
+            <View
+                style={{
+                    height: 1,
+                    backgroundColor: "#4e1b4e",
+                }}
+            />
+            <FlatList data={pizzarias_data} renderItem={render_list_item}
+                      style={{paddingTop: 28, paddingBottom: 24}}/>
+        </>
     )
 }
 
@@ -147,7 +158,6 @@ const styles = StyleSheet.create({
     card: {
         paddingHorizontal: 24,
         paddingVertical: 12,
-        backgroundColor: "#3c77f5",
         borderWidth: 1,
         borderColor: "#3c77f5",
         borderRadius: "10px",
